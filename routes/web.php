20eabb9d575265e38f1819ad8811086441345339
Joost van Someren <joost.v.someren@gmail.com>
2018-04-19 09:55:13 +0200

    Add Authentication routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,16 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+$router->group(['prefix' => 'auth'], function() use ($router) {
+    $router->post('addAdmin', ['uses' => 'AuthController@addAdmin']);
+    $router->post('clearSessions', ['uses' => 'AuthController@clearSessions']);
+    $router->post('deleteAdmin', ['uses' => 'AuthController@deleteAdmin']);
+    $router->post('isAdmin', ['uses' => 'AuthController@isAdmin']);
+    $router->post('login', ['uses' => 'AuthController@login']);
+    $router->post('logout', ['uses' => 'AuthController@logout']);
+    $router->post('validateCookies', ['uses' => 'AuthController@validateCookies']);
+});
+
 $router->group(['prefix' => 'resources'], function () use ($router) {
     $router->get('', ['uses' => 'ResourceController@getAllResources']);
     $router->get('{id}', ['uses' => 'ResourceController@getResource']);
